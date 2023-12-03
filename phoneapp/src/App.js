57725874e7dd0cfddfb641d11353b268040847e4
deepak <dkp041297@gmail.com>
2023-12-03 09:14:27 +0530

    task1

---
+++ phoneapp/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import AddPost from "./components/AddContact";
+import EditContact from "./components/EditContact";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import "./styles.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <ToastContainer />
+      <Navbar />
+      <Routes>
+      <Route  path="/" element={ <Home />} />
+      <Route  path="/add" element={ <AddPost />} />
+      <Route  path="/edit/:id" element={<EditContact />} />
+      </Routes>
+    </div>
+  );
+};
+export default App;
